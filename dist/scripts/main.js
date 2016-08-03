c872b419ba188ecdea35f17464c370f4450a18b2
Jolja06 <elena.an.92@gmail.com>
2016-08-04 00:59:46 +0300

    create dist directory

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,57 +1,82 @@
-angular.module('calcApp', []);
-angular.module('calcApp').controller('ProdCtrl', function($scope) {
-  
-  // Инициализация скидки
-  $scope.discount = 7;
-  
-  // Инициализация товаров
-  $scope.products = [
-  ];
-
-  // Функцию, которая отвечает за добавление товара  
-  $scope.addNewProd = function() {
-    $scope.products.push($scope.newProd);
-    $scope.newProd.sale = $scope.newProd.price;
-    $scope.newProd = null;
-  };
-
-  // Функция, которые высчитывает скидку
-  $scope.discounApply = function() {
-
-    // Сумма товаров в корзине
-    var sum = 0;
-    for( var i = 0; i < $scope.products.length; i++ ) {
-      sum += $scope.products[i].price;
-    };
-
-    // Вычисляется процент каждого товара от общей стоимости
-    // Вычисление скидки пропорционально от каждого товара с округлением в меньшую сторону
-    var number = [];
-    for( var j = 0; j <$scope.products.length; j++ ) {
-      number.push(Math.floor($scope.discount*(($scope.products[j].price)*100/sum)/100));
-    };
-
-    // Вычисление суммы скидки, которая уже расписана по товарам
-    var sale = number.reduce(function(sum, current) {
-      return sum + current;
-    }, 0); 
-
-    // Поиск индекса товара с самой высокой ценой 
-    for( var a = 0; a < number.length; a++ ) {
-      var emax = number[a];
-      if(number[a] < emax) {
-        emax = number[a];
-      }
-    }
-
-    // Замена значения скидки для самого дорогого товара, 
-    // оставшаяся после округления, чтобы в итоге была скидка равная $scope.discount = 7;
-    number[emax] = number[emax] + ($scope.discount - sale);
+(function(){
+  angular
+    .module('calcApp', ['discount.service', 'products.ctrl']);
+})();
+(function() {
+  angular
+    .module('products.ctrl', [])
+    .controller('ProdCtrl', function($scope, DiscountService) {
     
-    // Вывод скидки для каждого товара
-    for( var m = 0; m < $scope.products.length; m++ ) {
-      var sale = $scope.products[m].sale;
-      $scope.products[m].sale = $scope.products[m].price - number[m];
-    }
-  }
-});
+      // Инициализация скидки
+      $scope.discount = 7;
+      
+      // Инициализация товаров
+      $scope.products = [
+      ];
+
+      // Функцию, которая отвечает за добавление товара  
+      $scope.addNewProd = function() {
+        $scope.products.push($scope.newProd);
+        $scope.newProd.sale = $scope.newProd.price;
+        $scope.newProd = null;
+      };
+
+      // Функция, которые высчитывает скидку
+      $scope.discounApply = function() {
+        DiscountService.count($scope.products, $scope.discount);
+      }
+    });
+})();
+(function() {
+  angular
+    .module('discount.service', [])
+    .factory('DiscountService', DiscountService)
+
+    function DiscountService() {
+      var obj = {count: count};
+
+      function count(prod, discount) {
+        // Сумма товаров в корзине
+        var sum = 0;
+        for( var i = 0; i < prod.length; i++ ) {
+          sum += prod[i].price;
+        };
+
+        // Вычисляется процент каждого товара от общей стоимости
+        // Вычисление скидки пропорционально от каждого товара с округлением в меньшую сторону
+        var number = [];
+        for( var j = 0; j <prod.length; j++ ) {
+          number.push(Math.floor(discount*((prod[j].price)*100/sum)/100));
+        };
+
+        // Вычисление суммы скидки, которая уже расписана по товарам
+        var sale = number.reduce(function(sum, current) {
+          return sum + current;
+        }, 0); 
+
+        // Поиск индекса товара с самой высокой ценой 
+        var emax = Math.max.apply(null, number);
+
+        // Замена значения скидки для самого дорогого товара, 
+        // оставшаяся после округления, чтобы в итоге была скидка равная discount = 7;
+        var a = 0;
+        var index = 0;
+        for ( a; a < number.length; a++) {
+          if(number[a] === emax) {
+            index = a;
+          } 
+        }
+
+        number[index] = emax + (discount - sale);
+
+        // Вывод скидки для каждого товара
+        for( var m = 0; m < prod.length; m++ ) {
+          var sale = prod[m].sale;
+          prod[m].sale = prod[m].price - number[m];
+        }
+        return prod;
+      }
+      return obj;
+    } 
+
+})();
